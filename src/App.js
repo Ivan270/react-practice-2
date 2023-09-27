ab4b767c3d5823d5667b5fe8b027ec269a431845
Ivan Verdugo <ivan270@gmail.com>
2023-09-27 12:46:02 -0300

    finished error modal with input validation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
 import React, { useState } from 'react';
 import AddUser from './Components/AddUser/AddUser';
 import UsersList from './Components/UserList/UsersList';
+import ErrorModal from './Components/ErrorModal/ErrorModal';
 
 function App() {
 	const [users, setUsers] = useState([]);
+	const [modalShow, setModalShow] = useState('none');
+	const [modalTitle, setModalTitle] = useState('Error');
+	const [modalDescription, setModalDescription] =
+		useState('Something happened');
 
 	const addUserHandler = (userInput) => {
-		const user = {
-			...userInput,
-			id: Math.floor(Math.random() * 5),
-		};
-		const array = [...users, user];
-		setUsers(array);
+		// Validation
+		console.log(userInput);
+		if (userInput.username === '' || userInput.age === '') {
+			setModalShow('block');
+			setModalTitle('Invalid Input');
+			setModalDescription(
+				'Please enter a valid name and age (non-empty values)'
+			);
+		} else if (userInput.age < 0) {
+			setModalShow('block');
+			setModalTitle('Invalid age');
+			setModalDescription('Please enter a valid age (>0).');
+		} else {
+			const user = {
+				...userInput,
+				id: Math.random().toString(36).substring(2),
+			};
+			const array = [...users, user];
+			setUsers(array);
+		}
+	};
+
+	const dismissModal = (disp) => {
+		setModalShow(disp);
 	};
 
 	let content = '';
@@ -21,6 +44,12 @@ function App() {
 
 	return (
 		<div>
+			<ErrorModal
+				title={modalTitle}
+				description={modalDescription}
+				onModalClick={dismissModal}
+				display={{ display: modalShow }}
+			/>
 			<AddUser onSubmitUser={addUserHandler} />
 			{/* Conditional render*/}
 			{content}
--- src/Components/ErrorModal/ErrorModal.js
+++ src/Components/ErrorModal/ErrorModal.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import Button from '../Button/Button';
+import Card from '../Card/Card';
 import styles from './ErrorModal.module.css';
 
-const ErrorModal = () => {
-	return <h5>Error modal comp</h5>;
+const ErrorModal = (props) => {
+	const modalClickHandler = () => {
+		props.onModalClick('none');
+	};
+
+	return (
+		<div
+			onClick={modalClickHandler}
+			style={props.display}
+			className={styles.backdrop}
+		>
+			<div className={styles.modal}>
+				<Card>
+					<header className={styles.header}>
+						<h2>{props.title}</h2>
+					</header>
+					<div className={styles.content}>
+						<p>{props.description}</p>
+					</div>
+					<div className={styles.actions}>
+						<Button onClick={modalClickHandler}>Okay</Button>
+					</div>
+				</Card>
+			</div>
+		</div>
+	);
 };
 
 export default ErrorModal;
